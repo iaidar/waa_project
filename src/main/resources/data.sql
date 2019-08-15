721f2e609573912100106ec15b473a51a994ec5e
Younes <by_sennadj@esi.dz>
2019-08-15 06:59:45 -0500

    - Remove predefined data queries

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,28 +13,5 @@ INSERT INTO User VALUES (0,1,'webstorewaa@gmail.com','$2a$10$5yC4meSZGDFC/bEWquh
 INSERT INTO User_role VALUES (1,0);
 
 -- for tests
-INSERT INTO cart VALUES (10020);
-INSERT INTO cart VALUES (10021);
 
-INSERT INTO User VALUES (10000,1,'by_sennadj@esi.dz','$2a$10$5yC4meSZGDFC/bEWquhkAe.3B6B0kk5GJcu4dTN3O52G0jiBdauVa','buyer');
-INSERT INTO User_role VALUES (3,10000);
-INSERT INTO buyer VALUES (10001,0,10020,10000);
-
-
-
-INSERT INTO User VALUES (10002,1,'ysennadj@mum.edu','$2a$10$5yC4meSZGDFC/bEWquhkAe.3B6B0kk5GJcu4dTN3O52G0jiBdauVa','buyer2');
-INSERT INTO User_role VALUES (3,10002);
-INSERT INTO buyer VALUES (10003,0,10021,10002);
-
-INSERT INTO User VALUES (10004,1,'by_sennadj@esi.dz','$2a$10$5yC4meSZGDFC/bEWquhkAe.3B6B0kk5GJcu4dTN3O52G0jiBdauVa','seller1');
-INSERT INTO User_role VALUES (2,10004);
-INSERT INTO seller VALUES (10005,10004);
-
-INSERT INTO User VALUES (10006,1,'by_sennadj@esi.dz','$2a$10$5yC4meSZGDFC/bEWquhkAe.3B6B0kk5GJcu4dTN3O52G0jiBdauVa','seller2');
-INSERT INTO User_role VALUES (2,10006);
-INSERT INTO seller VALUES (10007,10006);
-
-INSERT INTO product VALUES (10008,1,'description 1',10,'product 1',10005);
-INSERT INTO product VALUES (10009,1,'description 2',20,'product 2',10005);
-INSERT INTO product VALUES (10010,1,'description 3',30,'product 3',10007);
 
